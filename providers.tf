adee4d65b5e0958f12e94bc936dee8a1254d24df
David Redmin <david.redmin@trio.dhs.gov>
2021-08-13 16:02:42 -0400

    Set up providers

--- providers.tf
+++ providers.tf
@@ -1,3 +1,37 @@
-# Default provider information.
+# This is the "default" provider that is used to obtain the caller's
+# credentials, which are used to set the session name when assuming roles in
+# the other providers.
 provider "aws" {
+  default_tags {
+    tags = var.tags
+  }
+  region = var.aws_region
+}
+
+# The provider used to determine which accounts in the organization are
+# assessment accounts and which are not (OrganizationsReadOnly role in
+# Master account).
+provider "aws" {
+  alias = "organizationsreadonly"
+  assume_role {
+    role_arn     = data.terraform_remote_state.master.outputs.organizationsreadonly_role.arn
+    session_name = local.caller_user_name
+  }
+  default_tags {
+    tags = var.tags
+  }
+  region = var.aws_region
+}
+
+# The provider used to create resources inside the Users account.
+provider "aws" {
+  alias = "users"
+  assume_role {
+    role_arn     = data.terraform_remote_state.users.outputs.provisionaccount_role.arn
+    session_name = local.caller_user_name
+  }
+  default_tags {
+    tags = var.tags
+  }
+  region = var.aws_region
 }
