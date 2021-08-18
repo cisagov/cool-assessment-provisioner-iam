fe78efdbd9ab1d31c35b346d0e5f5973af6acbdf
David Redmin <david.redmin@trio.dhs.gov>
2021-08-17 21:17:09 -0400

    Add new policy statement allowing assumption of key non-assessment roles
    Also:
    * Only deny prohibited non-assessment provision roles (as opposed to all 
    non-assessment provision roles)
    * Update sids to be more specific
    * Update comments to be more accurate
    * Alphabetize statements in policy (by sid)

--- policies.tf
+++ policies.tf
@@ -1,19 +1,7 @@
-# The policy document that allows assumption of the provisioner role in
-# assessment accounts only.
+# The policy document that allows assumption of all roles needed in order to
+# provision assessment environments.  It also specifically denies assumption
+# of non-required non-assessment account provisioning roles.
 data "aws_iam_policy_document" "provision_assessment" {
-  statement {
-    actions = [
-      "sts:AssumeRole",
-      "sts:TagSession",
-    ]
-
-    effect = "Deny"
-
-    resources = formatlist("arn:aws:iam::%s:role/%s", local.non_assessment_account_ids, var.provision_assessment_role_name)
-
-    sid = "DenyNonAssessmentAccounts"
-  }
-
   statement {
     actions = [
       "sts:AssumeRole",
@@ -36,12 +24,46 @@ data "aws_iam_policy_document" "provision_assessment" {
       "arn:aws:iam::*:role/${var.provision_assessment_role_name}",
     ]
 
-    sid = "AllowAssessmentAccounts"
+    sid = "AllowAssessmentAccountRoles"
+  }
+
+  statement {
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      data.terraform_remote_state.dns_certboto.outputs.provisioncertificatereadroles_role.arn,
+      data.terraform_remote_state.images_parameterstore-production.outputs.provisionparameterstorereadroles_role.arn,
+      data.terraform_remote_state.images_parameterstore-staging.outputs.provisionparameterstorereadroles_role.arn,
+      data.terraform_remote_state.master.outputs.organizationsreadonly_role.arn,
+      data.terraform_remote_state.sharedservices-production.outputs.provisionaccount_role.arn,
+      data.terraform_remote_state.sharedservices-staging.outputs.provisionaccount_role.arn,
+      data.terraform_remote_state.terraform.outputs.access_terraform_backend_role.arn,
+    ]
+
+    sid = "AllowRequiredNonAssessmentAccountRoles"
+  }
+
+  statement {
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession",
+    ]
+
+    effect = "Deny"
+
+    resources = local.prohibited_non_assessment_provision_roles
+
+    sid = "DenyNonAssessmentAccountRoles"
   }
 }
 
-# The policy that allows assumption of the provisioner role in assessment
-# accounts only.
+# The policy that allows assumption of all roles needed in order to provision
+# assessment environments.
 resource "aws_iam_policy" "provision_assessment" {
   provider = aws.users
 
