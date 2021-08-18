defb079cde166bba6a98649e145b5980a6a499f3
David Redmin <david.redmin@trio.dhs.gov>
2021-08-17 21:08:59 -0400

    Add additional remote states
    These contain outputs of various roles that are required in order to 
    successfully provision assessment environments.

--- remote_states.tf
+++ remote_states.tf
@@ -4,6 +4,53 @@
 # input data for this configuration.
 # ------------------------------------------------------------------------------
 
+data "terraform_remote_state" "dns_certboto" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-dns-certboto/terraform.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to set up "production" and "staging" remote states.
+  workspace = "production"
+}
+
+data "terraform_remote_state" "images_parameterstore-production" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-images-parameterstore/terraform.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "images_parameterstore-staging" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-images-parameterstore/terraform.tfstate"
+  }
+
+  workspace = "staging"
+}
+
 data "terraform_remote_state" "master" {
   backend = "s3"
 
@@ -17,7 +64,54 @@ data "terraform_remote_state" "master" {
   }
 
   # There is only one environment for this account, so there is
-  # no need to match the current Terraform workspace.
+  # no need to set up "production" and "staging" remote states.
+  workspace = "production"
+}
+
+data "terraform_remote_state" "sharedservices-production" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/shared_services.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "sharedservices-staging" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/shared_services.tfstate"
+  }
+
+  workspace = "staging"
+}
+
+data "terraform_remote_state" "terraform" {
+  backend = "s3"
+
+  config = {
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+    key            = "cool-accounts/terraform.tfstate"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to set up "production" and "staging" remote states.
   workspace = "production"
 }
 
@@ -34,6 +128,6 @@ data "terraform_remote_state" "users" {
   }
 
   # There is only one environment for this account, so there is
-  # no need to match the current Terraform workspace.
+  # no need to set up "production" and "staging" remote states.
   workspace = "production"
 }
