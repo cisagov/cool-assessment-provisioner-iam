9d3c5adeec0eb89d7e3cbf93b92533273977983c
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-07 13:47:30 -0400

    Alphabetize

--- group_membership.tf
+++ group_membership.tf
@@ -3,9 +3,8 @@ resource "aws_iam_user_group_membership" "assessment_provisioners" {
   provider = aws.users
   for_each = toset(var.users)
 
-  user = each.key
-
   groups = [
     aws_iam_group.assessment_provisioners.name
   ]
+  user = each.key
 }
